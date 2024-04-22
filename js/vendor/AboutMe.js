e4b75908397861dcf66ea841a11463a7e5e8df77
Donnelly Earley <donnelly@etruckbiz.com>
2024-04-22 15:06:18 -0500

    website progress

--- js/vendor/AboutMe.js
+++ js/vendor/AboutMe.js
@@ -1,12 +1,18 @@
 const theme = localStorage.getItem("theme");
 var darkMode = false;
 
+if(theme === 'dark'){
+  darkMode = !darkMode;
+}
+
 function toggleDarkMode() {
   const themeImage = document.getElementById("theme-image");
   const links = document.querySelectorAll(".links a");
   const codeSquare = document.getElementById("code-square");
 
   if (darkMode) {
+    localStorage.setItem("theme", "light");
+
     //dark mode local variable change
     darkMode = false;
 
@@ -14,16 +20,21 @@ function toggleDarkMode() {
     document.body.classList.remove("dark-mode");
 
     //theme button toggle icon change
-    themeImage.src = 'img/moon-fill.svg';
+    themeImage.src = '../img/moon-fill.svg';
 
     //nav bar links change
     links.forEach(function (link) {
-      link.style.color = "#333";
+      if(link.className === 'current'){
+        link.style.color = "#333";
+        link.style['border-bottom'] = '2px solid #333';
+      }
     });
 
     //code square color change
-    codeSquare.src = "img/code-square-dark.svg";
+    codeSquare.src = "../img/code-square-dark.svg";
   } else {
+    localStorage.setItem("theme", "dark");
+
     //dark mode local variable change
     darkMode = true;
 
@@ -31,14 +42,17 @@ function toggleDarkMode() {
     document.body.classList.add("dark-mode");
 
     //theme button toggle icon change
-    themeImage.src = "img/sun-fill.svg";
+    themeImage.src = "../img/sun-fill.svg";
 
     //nav bar links change
     links.forEach(function (link) {
-      link.style.color = "white";
+      if(link.className === 'current'){
+        link.style.color = "white";
+        link.style['border-bottom'] = '2px solid white';
+      }
     });
 
     //code square color change
-    codeSquare.src = "img/code-square-light.svg";
+    codeSquare.src = "../img/code-square-light.svg";
   }
 }
--- js/vendor/linkToggle.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const links = document.querySelectorAll(".links li a");
-
-  links.forEach(function (link) {
-    if (link.href === window.location.href) {
-      link.parentElement.classList.add("current");
-    }
-  });
-});
